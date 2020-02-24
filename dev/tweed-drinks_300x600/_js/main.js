2d5041c09000c8d0e9eb9892d169fde0f7b94f3b
Gar <gar@lonelydatu.com>
2020-02-24 10:51:06 -0600

    one

---
+++ dev/tweed-drinks_300x600/_js/main.js
@@ -0,0 +1,43 @@
+
+
+function start(){
+	const tl = new TimelineMax()
+	tl.set(".frame1", {opacity:1})
+	
+	tl.from('.t1', .3, {opacity:0})
+	tl.to('.t1', .3, {opacity:0}, "+=3")
+
+
+	tl.set(".frame2", {opacity:1})
+	tl.from('.t2', .3, {opacity:0})
+	tl.from('.product_1', .3, {opacity:0}, "+=.3")
+	tl.to('.frame2', .3, {opacity:0}, "+=3")
+
+	tl.set(".frame3", {opacity:1})
+	tl.from('.t3', .3, {opacity:0})
+	tl.from('.product_2', .3, {opacity:0}, "+=.3")
+	tl.to('.frame3', .3, {opacity:0}, "+=3")
+
+
+	tl.set(".frame4", {opacity:1})
+	tl.from('.t4', .3, {opacity:0})
+	tl.from('.product_3', .3, {opacity:0}, "+=.3")
+	
+
+	// tl.set(".frame3", {opacity:1})
+	// tl.from('.logo_text', .3, {opacity:0})
+	// tl.from('.cta', .3, {opacity:0}, "+=.2")
+
+	// tl.set(".frame3", {opacity:1})
+	// tl.from('.product', .3, {opacity:0})
+	// tl.to('.product', .3, {opacity:0}, "+=3")
+
+}
+
+start()
+
+
+
+
+module.exports = {};
+
