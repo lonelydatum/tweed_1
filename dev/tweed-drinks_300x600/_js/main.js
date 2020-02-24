6dc525cb9436b7ca5c4f85b10f32cd3bdc83b81b
Gar <gar@lonelydatu.com>
2020-02-24 10:53:34 -0600

    one

--- dev/tweed-drinks_300x600/_js/main.js
+++ dev/tweed-drinks_300x600/_js/main.js
@@ -5,18 +5,18 @@ function start(){
 	tl.set(".frame1", {opacity:1})
 	
 	tl.from('.t1', .3, {opacity:0})
-	tl.to('.t1', .3, {opacity:0}, "+=3")
+	tl.to('.t1', .3, {opacity:0}, "+=2")
 
 
 	tl.set(".frame2", {opacity:1})
 	tl.from('.t2', .3, {opacity:0})
 	tl.from('.product_1', .3, {opacity:0}, "+=.3")
-	tl.to('.frame2', .3, {opacity:0}, "+=3")
+	tl.to('.frame2', .3, {opacity:0}, "+=2")
 
 	tl.set(".frame3", {opacity:1})
 	tl.from('.t3', .3, {opacity:0})
 	tl.from('.product_2', .3, {opacity:0}, "+=.3")
-	tl.to('.frame3', .3, {opacity:0}, "+=3")
+	tl.to('.frame3', .3, {opacity:0}, "+=2")
 
 
 	tl.set(".frame4", {opacity:1})
@@ -30,7 +30,7 @@ function start(){
 
 	// tl.set(".frame3", {opacity:1})
 	// tl.from('.product', .3, {opacity:0})
-	// tl.to('.product', .3, {opacity:0}, "+=3")
+	// tl.to('.product', .3, {opacity:0}, "+=2")
 
 }
 
